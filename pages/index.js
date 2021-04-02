c7147f1b5a4f52fc1cdb3cd0d9c25fbc4069dc2f
Yanick Landry <yanick.landry@gmail.com>
2021-04-02 18:39:42 -0400

    replaced yarn by npm, added furioos, antd, button

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,30 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Player } from 'furioos-sdk';
+import { useState, useEffect } from 'react';
+import { Button } from 'antd'
+import { FullscreenOutlined } from '@ant-design/icons'
+
+const options = {
+  whiteLabel: true,
+  hideToolbar: false,
+  hideTitle: true,
+  hidePlayButton: false,
+};
 
 export default function Home() {
+  const [player, setPlayer] = useState(null)
+
+  useEffect(() => {
+    const p = new Player("422.037", "furioos_container", options);
+    setPlayer(p);
+  }, [])
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Button type="primary" icon={<FullscreenOutlined />} size="large" onClick={() => player.maximize()}>
+        Fullscreen
+      </Button>
+      <div
+        id="furioos_container" />
     </div>
   )
 }
